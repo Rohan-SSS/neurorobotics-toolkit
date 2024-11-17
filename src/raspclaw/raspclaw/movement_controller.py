79a1b9732f6ef1d115df3284d906c6fb37c154c0
shandilya1998 <anirudhsuraj01@gmail.com>
2024-11-17 16:00:25 +0530

    Corrections made to the movement_controller node
    Problem
    =======
    The move function is a low level function as such switched command_input
    function
    
    Solution
    ========
    changed move function to command_input function and renamed the class to
    suit the task and removed unecessary code.
    
    Note
    ====

--- src/raspclaw/raspclaw/movement_controller.py
+++ src/raspclaw/raspclaw/movement_controller.py
@@ -6,18 +6,17 @@ from pathlib import Path
 
 
 sys.path.append(str(Path('/ws/ros_ws/src/raspclaw/ext/raspclawsdk/server')))
-import move  
+from move import*
 
-class MinimalSubscriber(Node):
+class MovementController(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
+        super().__init__('MovementController') ## name should be the same as the described one 
+        self.subscription = self.create_subscription(# creates a subscriptions 
             Twist,
-            'cmd_vel',
+            'cmd_vel', ## default command velocity default topic for publishing velocty commands
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
         self.linear_threshold = 0.1
         self.angular_threshold = 0.1
 
@@ -30,33 +29,58 @@ class MinimalSubscriber(Node):
             
     '''
         method to conver twist messages into discrete commands using if-else
-        def twist_to_discreet(msg)
+def convert_twist_to_discrete(twist_msg):
+    linear_threshold = 0.1  
+    angular_threshold = 0.1  
+
+    
+    linear_velocity = twist_msg.linear.x  # Forward/backward speed
+    angular_velocity = twist_msg.angular.z  # Turning speed
+
+    left_command = 0
+    right_command = 0
+
+    # Check if the robot is moving forward or backward
+    if abs(linear_velocity) > linear_threshold:
+        if linear_velocity > 0:
+            # Moving forward
+            left_command = 1
+            right_command = 1
+        elif linear_velocity < 0:
+            # Moving backward
+            left_command = -1
+            right_command = -1
+
+    # Check if the robot is turning
+    if abs(angular_velocity) > angular_threshold:
+        if angular_velocity > 0:
+            # Turning right
+            left_command = 1
+            right_command = -1
+        elif angular_velocity < 0:
+            # Turning left
+            left_command = -1
+            right_command = 1
+
+    # Return discrete commands
+    return left_command, right_command
+
      '''     
     def process_twist(self, msg):
         # Define speed and step input based on Twist message
         speed = int(msg.linear.x * 100)  # Scale linear.x to a suitable speed range (example scaling)
-        step_input = 1  # Example step input; adjust as necessary
-        # turn right or letf based on agular.z
+        step_input = 1  # steps taken will be 1 at the speed specified 
+        # turn right or letf based on agular.z that is we check the angular.z value first and if it si 0 then we proceed to move it forward/back 
         if msg.angular.z > 0 :  # turning right
-            move.move(step_input, speed, 'right')
+            command_input('right')
         elif msg.angular.z < 0:  # turn left
-            move.move(step_input, speed, 'left')
+            command_input('left')
         else:  # move forward or backward based on linear.x
             if speed > 0:
-                move.move(step_input, speed, 'no')  # move forward
+                command_input('forward')  # move forward
             elif speed < 0:
-                move.move(step_input, -speed, 'no')  # move backward
+                command_input('backward')  # move backward
 
-def main(args=None):
-    rclpy.init(args=args)
-    minimal_subscriber = MinimalSubscriber()
-    rclpy.spin(minimal_subscriber)
-
-    minimal_subscriber.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
 
 
         
@@ -72,3 +96,5 @@ if __name__ == '__main__':
 ## task 4 - use one of the methods from move.py to move the servo using the node.
 ## move.py is in ros_ws->build->controllers->src->raspclaw->ext/raspclaw->server
 ## the setup.py file is in ros_ws->build->opencv_tests
+## moved the file 
+## ./scripts/build_ros_packages.sh this is from within the container for outside - djinn build nrt 
